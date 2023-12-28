5d5cf0865baed3e9297b7b713d4beb976a33648a
goga997 <grigore.rgg@gmail.com>
2023-12-28 15:06:28 +0200

    created LogIn and Registration UI

---
+++ GoMessage/Resources/Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  Extensions.swift
+//  GoMessage
+//
+//  Created by Grigore on 28.12.2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.origin.y + self.frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.origin.x + self.frame.size.width
+    }
+    
+    
+}
