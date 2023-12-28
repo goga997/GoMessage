b0aa5a69ece11282283f76f0e1e0e48eb22d6475
goga997 <grigore.rgg@gmail.com>
2023-12-28 13:18:43 +0200

    Getting Started

---
+++ GoMessage/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoMessage
+//
+//  Created by Grigore on 28.12.2023.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
---
+++ GoMessage/Controllers/PhotoViewerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoViewerViewController.swift
+//  GoMessage
+//
+//  Created by Grigore on 28.12.2023.
+//
+
+import UIKit
+
+class PhotoViewerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
